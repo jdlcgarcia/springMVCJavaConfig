f1815393d159ef088188db96cdfb03666b328bfd
jdlcgarcia <jcruzgar@gmail.com>
2019-04-19 18:58:55 +0200

    Proyecto completo tras vídeo explicación

---
+++ SpringMvcJava/src/main/java/com/jdlcgarcia/controller/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.jdlcgarcia.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.jdlcgarcia")
+public class MvcConfig extends WebMvcConfigurerAdapter
+{
+
+}
